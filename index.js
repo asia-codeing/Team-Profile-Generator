2df457b622b205e65394cf130d8b5776b525328f
Asia Alnahi <asia.alius@gmail.com>
2021-04-07 14:48:06 -0700

    Import the required packages and files to the index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//Import the backages
+const inquirer = require('inquirer');
+const fs       = require('fs');
+//Import the classes
+const Employee = require('./lib/Employee');
+const Manager  = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern   = require('./lib/Intern');
