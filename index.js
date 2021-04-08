0950aa960e75e921b49dd6a5b325f653923f0e10
Asia Alnahi <asia.alius@gmail.com>
2021-04-08 12:28:09 -0700

    Created file that contain the functions creat html that will get generated

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Employee = require('./lib/Employee');
 const Manager  = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern   = require('./lib/Intern');
-const { listenerCount } = require('events');
 
 
 function addMember(){
---
+++ template/generateHTML.js
@@ -0,0 +1,78 @@
+const generateCards = teamInfo => {
+ 
+    const manager = teamInfo.manager.map(function(job) {
+        let managerHtml = `
+        <div class="card card-column">
+            <div class="card-header">
+                <h2>${job.name}</h2>
+                <h4 class="job-title">Manager</h4>
+            </div>
+            <div class="card-body">
+                <p class="e-ID">ID: ${job.id}</p>
+                <p class="email">Email: <a href="mailto:${job.email}">${job.email}</a></p>
+                <p class="office-no">Office Number: ${job.office}</p>
+            </div> 
+        </div>
+        `
+        return managerHtml
+    });
+
+    const engineer = teamInfo.engineer.map(function(job) {
+        let engineerHtml = `
+        <div class="card card-column">
+            <div class="card-header">
+                <h2>${job.name}</h2>
+                <h4 class="job-title">Engineer</h4>
+            </div>
+        <div class="card-body">
+                <p class="e-ID">ID: ${job.id}</p>
+                <p class="email">Email: <a href="mailto:${job.email}">${job.email}</a></p>
+                <p class="github">GitHub Username: <a href="https://github.com/${job.github}" target="_blank" rel="noopener noreferrer">${job.github}</a></p>
+            </div> 
+        </div>
+        `
+        return engineerHtml
+    })
+
+    const intern = teamInfo.intern.map(function(job) {
+        let interHtml = `
+        <div class="card card-column">
+            <div class="card-header">
+                <h2>${job.name}</h2>
+                <h4 class="job-title">Intern</h4>
+            </div>
+            <div class="card-body">
+                <p class="e-ID">ID: ${job.id}</p>
+                <p class="email">Email: <a href="mailto:${job.email}">${job.email}</a></p>
+                <p class="school">School: ${job.school}</p>
+            </div> 
+        </div>
+        `
+        return interHtml
+    })
+    return [manager,engineer,intern]
+}
+
+module.exports = templateInfo => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="./styel.css">
+            <title>My Team</title>
+        </head>
+        <body>
+            <header>
+                <h1 class="header-title">My Team</h1>
+            </header>
+            <main>
+                <div id="card-row">
+                  ${generateCards(templateInfo)}
+                </div>
+            </main>
+        </body>
+        </html>    
+      `
+}
