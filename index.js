01543f74f804e7a7e12d12be46a161a9113c2c0f
Asia Alnahi <asia.alius@gmail.com>
2021-04-08 16:01:14 -0700

    Added the offficeNumber, GithubUser and School info to the employees

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const Employee = require('./lib/Employee');
 const Manager  = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern   = require('./lib/Intern');
+const GenerateHTML = require('./template/generateHTML')
 
+let manager = [];
+let engineer = [];
+let intern = [];
+let employeesArr = {manager, engineer, intern};
 
 function addMember(){
     inquirer.prompt([
@@ -18,7 +23,7 @@ function addMember(){
         {
             type: 'list',
             name: 'role',
-            choices: ['Manager',' Engineer','Intern'],
+            choices: ['Manager','Engineer','Intern'],
             message: "Select team member's role"
         },
 
@@ -33,10 +38,108 @@ function addMember(){
         message: "Enter team member's email address",
         name: "email"
         }
-    ])
+    ]).then(({name, id, email, role}) => {
+        if (role === 'Manager'){
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'office',
+                    message:"What is the Manager's office number?"
+                },
+                {
+                    type:'confirm',
+                    name:'nextEntery',
+                    message:'Do you like to add more employee?',
+                    default: false
 
+                }
+            ]).then(({office, nextEntery}) =>{
+                manager.push(new Manager(name, id, email, office));
+                if(nextEntery){
+                    return addMember();
+                }
+            })
 
-    }
+        } else if (role === 'Engineer'){
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'github',
+                    message:"Please enter emplyee's GitHub username: "
+                },
+                {
+                    type:'confirm',
+                    name:'nextEntery',
+                    message:'Do you like to add more employee?',
+                    default: false
 
+                }
+            ]).then(({github, nextEntery}) =>{
+                engineer.push(new Engineer(name, id, email, github));
+                if(nextEntery){
+                    return addMember();
+                }
+            })
+        } else if (role === 'Intern'){
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'school',
+                    message:"Please enter Intern's school:"
+                },
+                {
+                    type:'confirm',
+                    name:'nextEntery',
+                    message:'Do you like to add more employee?',
+                    default: false
 
+                }
+            ]).then(({school, nextEntery}) =>{
+                intern.push(new Intern(name, id, email, school));
+                if(nextEntery){
+                    return addMember();
+                }
+            })
+        }
+    })
 }
+
+// fs.writeFile('./output/index.html', GenerateHTML(employeesArr), function (err) {
+//     if (err) throw err;
+//     console.log('File is created successfully.');
+// });
+    
+// A function to write HTML file
+const createdFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./output/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message:'File created!'
+            })
+        })
+        addMember()
+
+.then(fileContent => {
+    return GenerateHTML(employeesArr)
+})
+.then(pageHTML => {
+    return createdFile(pageHTML)
+});
+    })
+    
+}
+
+
+// Function call to initialize app
+addMember()
+
+
+
+
