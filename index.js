007d55a34b423df0daab9a6779e3cda6d9dc45d4
Asia Alnahi <asia.alius@gmail.com>
2021-04-08 10:55:08 -0700

    created addMember function to promt the questions

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ const Employee = require('./lib/Employee');
 const Manager  = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern   = require('./lib/Intern');
+const { listenerCount } = require('events');
+
+
+function addMember(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter team member's name:"
+        },
+        {
+            type: 'list',
+            name: 'role',
+            choices: ['Manager',' Engineer','Intern'],
+            message: "Select team member's role"
+        },
+
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter team member's ID:"
+        },
+
+        {
+        type: 'input',
+        message: "Enter team member's email address",
+        name: "email"
+        }
+    ])
+
+
+    }
+
+
+}
